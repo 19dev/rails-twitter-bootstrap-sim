8572b4377685e08b9325f85a39138c2e7d62593a
nurettin senyer <seyyah@bil.omu.edu.tr>
2012-03-15 17:38:33 +0200

    ilk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTwitterBootstrapSim::Application.config.secret_token = 'e91a6c0ca54afac1e57291eab9176c7f93356e5bb209a6e7d691cc7ce00cb1a02819a0772f75eec3fd4e2c5292c316bc899fbb42fd156268fe15fb3a0880c6d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsTwitterBootstrapSim::Application.config.session_store :cookie_store, key: '_rails-twitter-bootstrap-sim_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsTwitterBootstrapSim::Application.config.session_store :active_record_store
